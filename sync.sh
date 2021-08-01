8f6a95ee851cef0245bc8fdeccf507c70b8652ff
Alexwcrafter <kontakt@ayokaacr.de>
2021-08-01 15:30:19 +0200

    Removed reeference in sync.sh

--- sync.sh
+++ sync.sh
@@ -17,7 +17,7 @@ cores=$(nproc --all)
 if [ "${cores}" -gt "12" ]; then
     cores=12
 fi
-repo sync --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune "-j${cores}" -c -v --reference /home/build/line/releases-1/rom
+repo sync --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune "-j${cores}" -c -v
 syncsuccessful="${?}"
 SYNC_END=$(date +"%s")
 SYNC_DIFF=$((SYNC_END - SYNC_START))
