82f8ad25d9e03a267f3b43491d2bce23b0027874
alexwcrafter <39865767+alexwcrafter@users.noreply.github.com>
2021-08-10 11:42:41 +0200

    Changed to Build Montana

--- config.sh
+++ config.sh
@@ -3,13 +3,13 @@
 export GITHUB_USER="alexwcrafter"
 export GITHUB_EMAIL="kontakt@ayokaacr.de"
 
-export device="sofiap"
+export device="montana"
 
 export ROM="StellarOS"
 export ROM_DIR="${WORKSPACE}/rom"
-export ROM_VERSION="11"
+export ROM_VERSION="S1"
 export official="false"
-export local_manifest_url="https://github.com/ACRBuilds/local_manifests/raw/main/line-sofiar.xml"
+export local_manifest_url="https://github.com/JarlPenguin/manifests/raw/master/lineage/18.x/msm8937-common.xml"
 export manifest_url="https://github.com/Stellar-OS/android"
 export rom_vendor_name="lineage"
 export branch="stellar-S1"
