d5c4a7f2c980f6999a6b1a1a43a5d399e25a7874
Nick <nift4@protonmail.com>
2021-08-22 08:31:10 +0200

    build: nougat

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ if [ "${clean}" == "clean" ]; then
 elif [ "${clean}" == "installclean" ]; then
     make installclean
 fi
-make "${bacon}"
+make "${bacon}" -j$(nproc --all)
 BUILD_END=$(date +"%s")
 BUILD_DIFF=$((BUILD_END - BUILD_START))
 
