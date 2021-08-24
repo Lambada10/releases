94e5520952a4a5436d2f0fd77a76f8673e4b4a21
alexwcrafter <39865767+alexwcrafter@users.noreply.github.com>
2021-08-24 22:19:28 +0200

    Revert: Changed to build clean

--- config.sh
+++ config.sh
@@ -14,7 +14,7 @@ export rom_vendor_name="lineage"
 export branch="cm-14.1"
 export bacon="bacon"
 export buildtype="userdebug"
-export clean="clean"
+export clean="installclean"
 export generate_incremental=""
 export upload_recovery="false"
 
