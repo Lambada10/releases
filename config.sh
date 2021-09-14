09bd92fe91f5e25d4351e0fbf690b3e4f53f0c5c
alexwcrafter <kontakt@ayokaacr.de>
2021-09-14 09:33:53 +0200

    Changed to "clean"

--- config.sh
+++ config.sh
@@ -14,7 +14,7 @@ export rom_vendor_name="stellar"
 export branch="stellar-S1-staging"
 export bacon="bacon"
 export buildtype="userdebug"
-export clean="installclean"
+export clean="clean"
 export generate_incremental=""
 export upload_recovery="false"
 
